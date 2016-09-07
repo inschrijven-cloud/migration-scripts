8f5c18894e29a4e5be110c7ca8474538b4c1a8f9
Thomas Toye <toye.thomas@gmail.com>
2016-09-07 23:13:56 +0200

    Insert crew data from db_recreate script

--- db_recreate.sh
+++ db_recreate.sh
@@ -5,5 +5,6 @@ curl -X PUT http://127.0.0.1:5984/some-db
 
 ./pg_dump_children.sh | ./normalize_children.sh | curl -X POST http://127.0.0.1:5984/some-db/_bulk_docs -d @- -H 'Content-Type: application/json'
 ./pg_dump_shifts.sh | ./normalize_shifts.sh | curl -X POST http://127.0.0.1:5984/some-db/_bulk_docs -d @- -H 'Content-Type: application/json'
+./pg_dump_crew.sh | ./normalize_crew.sh | curl -X POST http://127.0.0.1:5984/some-db/_bulk_docs -d @- -H 'Content-Type: application/json'
 
 
