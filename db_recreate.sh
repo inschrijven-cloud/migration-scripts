396d7f3f30329d82289a588f5a61daee9c6d35c1
Thomas Toye <toye.thomas@gmail.com>
2016-11-20 00:45:35 +0100

    Extract CouchDB tenant name to variable in DB recreate script

--- db_recreate.sh
+++ db_recreate.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+COUCHDB_DB_NAME="tenant-data-sometenant"
+
 curl -X DELETE http://127.0.0.1:5984/some-db
 curl -X PUT http://127.0.0.1:5984/some-db
 
-./pg_dump_children.sh | ./normalize_children.sh | curl -X POST http://127.0.0.1:5984/some-db/_bulk_docs -d @- -H 'Content-Type: application/json'
-./pg_dump_shifts.sh | ./normalize_shifts.sh | curl -X POST http://127.0.0.1:5984/some-db/_bulk_docs -d @- -H 'Content-Type: application/json'
-./pg_dump_crew.sh | ./normalize_crew.sh | curl -X POST http://127.0.0.1:5984/some-db/_bulk_docs -d @- -H 'Content-Type: application/json'
-
+./pg_dump_children.sh | ./normalize_children.sh | curl -X POST http://127.0.0.1:5984/$COUCHDB_DB_NAME/_bulk_docs -d @- -H 'Content-Type: application/json'
+./pg_dump_shifts.sh | ./normalize_shifts.sh | curl -X POST http://127.0.0.1:5984/$COUCHDB_DB_NAME/_bulk_docs -d @- -H 'Content-Type: application/json'
+./pg_dump_crew.sh | ./normalize_crew.sh | curl -X POST http://127.0.0.1:5984/$COUCHDB_DB_NAME/_bulk_docs -d @- -H 'Content-Type: application/json'
 
