c5e1f3c01f362c8bf049a2b40303b6a5bb29c6f2
Thomas Toye <thomas@toye.io>
2017-04-26 22:01:14 +0200

    Update models

--- medical_files/normalize_medical_files.js
+++ medical_files/normalize_medical_files.js
@@ -14,34 +14,35 @@ const options = commandLineArgs(optionDefinitions)
 
 fs.readFile(options.input, (err, data) => {
   csv.parse(data, function(err, data){
-    console.log(JSON.stringify(data.map(normalizeChild)));
+    console.log(JSON.stringify( { docs: data.slice(1).map(normalizeChild) } ));
   });
 });
 
 const normalizeChild = (child) => {
   const toReturn =  {
-    kind: 'type/medicalFileImportedChild/v1',
+    kind: 'type/child/v1',
     doc: {
       firstName: child[2],
       lastName: child[1],
-      address: {
+      legacyAddress: {
         street: child[3].split(' ').slice(0, -1).join(' '),
         number: child[3].split(' ').slice(-1)[0],
-        zipCode: child[4].split(' ')[0],
+        zipCode: Number(child[4].split(' ')[0]),
         city: child[4].split(' ').slice(1).join(' ')
       },
-      contact: {
+      legacyContact: {
         phone: [],
         email: []
       },
       contactPeople: [],
+      legacyContactPeople: [],
       medicalInformation: {
         allergies: {
           allergies: [ child[19] ],
           extraInformation: child[20]
         },
-        diseases: {
-          diseases: child[21],
+        conditions: {
+          conditions: [ child[21] ],
           extraInformation: child[22]
         },
         other: child[23]
@@ -50,19 +51,15 @@ const normalizeChild = (child) => {
   };
 
   if(child[5]) {
-    toReturn.doc.contact.phone = [ { kind: 'landline', phoneNumber: child[5] } ];
+    toReturn.doc.legacyContact.phone = [ { kind: 'landline', phoneNumber: child[5] } ];
   }
 
   if(child[6]) {
     toReturn.doc.birthDate = {
-      day: child[6].split('/')[0],
-      month: child[6].split('/')[1],
-      year: child[6].split('/')[2]
+      day: Number(child[6].split('/')[0]),
+      month: Number(child[6].split('/')[1]),
+      year: Number(child[6].split('/')[2])
     }
-  }
-
-  if(child[7]) {
-    toReturn.doc.bloodType = child[7];
   }
 
   if(child[8]) {
@@ -70,15 +67,15 @@ const normalizeChild = (child) => {
   }
 
   if(child[9]) {
-    toReturn.doc.contactPeople.push(makeContactPerson(child[9], child[10], child[11]));
+    toReturn.doc.legacyContactPeople.push(makeContactPerson(child[9], child[10], child[11]));
   }
 
   if(child[12]) {
-    toReturn.doc.contactPeople.push(makeContactPerson(child[12], child[13], child[14]));
+    toReturn.doc.legacyContactPeople.push(makeContactPerson(child[12], child[13], child[14]));
   }
 
   if(child[15]) {
-    toReturn.doc.contactPeople.push(makeContactPerson(child[15], child[16], child[17]));
+    toReturn.doc.legacyContactPeople.push(makeContactPerson(child[15], child[16], child[17]));
   }
 
   if(child[18]) {
--- normalize_children.sh
+++ normalize_children.sh
@@ -17,41 +17,43 @@ process.stdin.on('end', () => {
   process.stdout.write(JSON.stringify( { docs: output } ));
 });
 
-var normalizeChild = (child) => {
-  var toReturn = {};
-  toReturn.doc = {};
+const normalizeChild = (child) => {
+  const toReturn = {
+    kind: 'type/child/v1',
+    _id: child.id,
 
-  toReturn.kind = 'type/child/v1';
-  toReturn._id = child.id;
-
-  toReturn.doc.firstName = child.first_name;
-  toReturn.doc.lastName = child.last_name;
-
-  toReturn.doc.contact = {};
-  toReturn.doc.contact.phone = [];
-
-  toReturn.doc.contact.email = [];
+    doc: {
+      firstName: child.first_name,
+      lastName: child.last_name,
+      legacyContact: {
+        phone: [],
+        email: []
+      },
+      legacyAddress: {},
+      medicalInformation: {},
+      contactPeople: []
+    }
+  };
 
   if(child.mobile_phone) {
-    toReturn.doc.contact.phone.push({ kind: 'mobile', phoneNumber: child.mobile_phone });
+    toReturn.doc.legacyContact.phone.push({ kind: 'mobile', phoneNumber: child.mobile_phone });
   }
 
   if(child.landline) {
-    toReturn.doc.contact.phone.push({ kind: 'landline', phoneNumber: child.landline });
+    toReturn.doc.legacyContact.phone.push({ kind: 'landline', phoneNumber: child.landline });
   }
 
-  toReturn.doc.address = {};
-
-  if(child.street) toReturn.doc.address.street = child.street;
-  if(child.street_number) toReturn.doc.address.number = child.street_number;
-  if(child.zip_code) toReturn.doc.address.zipCode = Number(child.zip_code);
-  if(child.city) toReturn.doc.address.city = child.city;
+  if(child.street) toReturn.doc.legacyAddress.street = child.street;
+  if(child.street_number) toReturn.doc.legacyAddress.number = child.street_number;
+  if(child.zip_code) toReturn.doc.legacyAddress.zipCode = Number(child.zip_code);
+  if(child.city) toReturn.doc.legacyAddress.city = child.city;
 
   if(child.birth_date) {
     const splitDate = child.birth_date.split("-");
     toReturn.doc.birthDate = { year: Number(splitDate[0]), month: Number(splitDate[1]), day: Number(splitDate[2]) };
   }
 
+  toReturn.doc.medicalInformation = {};
   return toReturn;
 };
 
